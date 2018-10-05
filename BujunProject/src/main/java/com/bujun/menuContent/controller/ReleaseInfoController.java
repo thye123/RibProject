42eef3b25dcc2682196ff00e213642e557c496bd
hrd202-19 <hrd202-19@202-19-PC>
2018-10-05 13:01:42 +0900

    vv

---
+++ BujunProject/src/main/java/com/bujun/menuContent/controller/ReleaseInfoController.java
@@ -0,0 +1,20 @@
+package com.bujun.menuContent.controller;
+
+import java.util.HashMap;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class ReleaseInfoController {
+	
+	@RequestMapping("/opdata") 
+	public String Opdata(@RequestParam HashMap<String, Object> map, Model model) {
+		
+		model.addAttribute("menu", map);
+		return "user/sub/sub07/opdata";
+	}
+	
+}
